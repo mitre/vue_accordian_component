8b68937edee1f10952765163ccc0a64d609233bb
Sturtevant, Alicia A. (ASTURTEVANT) <a.sturtevant4@gmail.com>
2019-07-17 13:55:39 -0400

    added prism code highlighting
    Signed-off-by: Sturtevant, Alicia A. (ASTURTEVANT) <a.sturtevant4@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuesax from "vuesax";
+import VueTailwind from "vue-tailwind";
 
-Vue.config.productionTip = false
+Vue.use(VueTailwind);
+Vue.config.productionTip = false;
+
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+import "material-icons/iconfont/material-icons.css";
+Vue.use(Vuesax);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
